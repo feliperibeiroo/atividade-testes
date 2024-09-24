246f25fdf5ae9f7c9a6ca5ad82a58575efebdb17
Skycosta <100355347+Skycosta@users.noreply.github.com>
2024-09-24 08:43:07 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import supabase, { SupabaseContext } from './database/database'; // Certifique-se de importar corretamente
 import LoginScreen from './LoginScreen';
 import HomeScreen from './HomeScreen';
 import ConsultasScreen from './Consultas';
@@ -10,66 +11,67 @@ import CadastroConsultaScreen from './CadastroConsulta';
 import CadastroSucessoScreen from './CadastroSucesso'; 
 import CadastroAtendenteScreen from './CadastroAtendente'; 
 import PacientesScreen from './Pacientes'; 
-import MedicosScreen from './Medicos'; // Importando a tela Medicos
+import MedicosScreen from './Medicos'; 
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen
-          name="Consultas"
-          component={ConsultasScreen}
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen
-          name="Pacientes"
-          component={PacientesScreen} 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen
-          name="Médicos" // Nome da tela Médicos
-          component={MedicosScreen} // Componente que você vai criar
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen
-          name="CadastroConsulta"
-          component={CadastroConsultaScreen} 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen
-          name="CadastroSucesso"
-          component={CadastroSucessoScreen} 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen
-          name="CadastroAtendente"
-          component={CadastroAtendenteScreen} 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen
-          name="CadastroPaciente"
-          component={CadastroPacienteScreen} 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen
-          name="CadastroMedico"
-          component={CadastroMedicoScreen} 
-          options={{ headerShown: false }} 
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SupabaseContext.Provider value={supabase}> {/* Fornecendo o supabase */}
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }} 
+          />
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }} 
+          />
+          <Stack.Screen
+            name="Consultas"
+            component={ConsultasScreen}
+            options={{ headerShown: false }} 
+          />
+          <Stack.Screen
+            name="Pacientes"
+            component={PacientesScreen} 
+            options={{ headerShown: false }} 
+          />
+          <Stack.Screen
+            name="Médicos"
+            component={MedicosScreen}
+            options={{ headerShown: false }} 
+          />
+          <Stack.Screen
+            name="CadastroConsulta"
+            component={CadastroConsultaScreen} 
+            options={{ headerShown: false }} 
+          />
+          <Stack.Screen
+            name="CadastroSucesso"
+            component={CadastroSucessoScreen} 
+            options={{ headerShown: false }} 
+          />
+          <Stack.Screen
+            name="CadastroAtendente"
+            component={CadastroAtendenteScreen} 
+            options={{ headerShown: false }} 
+          />
+          <Stack.Screen
+            name="CadastroPaciente"
+            component={CadastroPacienteScreen} 
+            options={{ headerShown: false }} 
+          />
+          <Stack.Screen
+            name="CadastroMedico"
+            component={CadastroMedicoScreen} 
+            options={{ headerShown: false }} 
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SupabaseContext.Provider>
   );
 }
-
